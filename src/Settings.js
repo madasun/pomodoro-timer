389af6545238afc0873fe430a004b8a1d0b6913d
Madison Gessner <madagessner@gmail.com>
2023-03-14 14:31:14 -0500

    Initialize project

---
+++ src/Settings.js
@@ -0,0 +1,9 @@
+function Settings() {
+  return <div style={{textAlign:'left'}}>settings
+  <label>work minutes:</label>
+  
+  <label>break minutes:</label>
+  </div>;
+}
+
+export default Settings;
